b49157f9dcf7e78b5b183f3b9c40b361aa990cb2
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-03-10 18:24:35 +1100

    hmm

--- src/align_genotype/scripts/picard_fixprim.py
+++ src/align_genotype/scripts/picard_fixprim.py
@@ -7,10 +7,7 @@ def run_fixmate(batch: hail_batch.Batch, bam_path: str, out_bam_path: str) -> Jo
 
     job = batch.new_job(
         'Picard FixMateInformation',
-        attributes={'tool': 'picard'},
     )
-
-    job.image(config.image_path('picard', '3.4.0-3'))
     job.memory('16Gi')
     job.storage('250Gi')
     job.cpu(4)
