e5d859fac67c2aece2ae2c4e9039b908d70fc1b9
Johnnyassaf <johnny.assaf@populationgenomics.org.au>
2026-03-11 09:50:24 +1100

    making it better

--- src/align_genotype/scripts/picard_fixprim.py
+++ src/align_genotype/scripts/picard_fixprim.py
@@ -1,75 +1,77 @@
 import argparse
-from cpg_utils import config, hail_batch
+from cpg_utils import hail_batch
 from hailtop.batch.job import Job
 
 def run_fixmate(batch: hail_batch.Batch, bam_path: str, out_bam_path: str) -> Job:
-    """Run Picard FixMateInformation on BAM file to clean mate information."""
+    job = batch.new_job('Fix Primary Alignments with Validation')
 
-    job = batch.new_job(
-        'Picard FixMateInformation',
-    )
-
-    # Use the custom image that has pysam installed
     job.image('australia-southeast1-docker.pkg.dev/cpg-common/images-dev/cpg-flow-align-genotype:0.4.5-2')
     job.memory('16Gi')
     job.storage('250Gi')
-    job.cpu(4)
+    job.cpu(8)
 
-    # read in the BAM and index
-    bam_localised = batch.read_input_group(
-        bam=bam_path,
-        bai=f'{bam_path}.bai',
-    ).bam
+    bam_localised = batch.read_input_group(bam=bam_path, bai=f'{bam_path}.bai')
 
-    # Declare output resource group
     job.declare_resource_group(
-        output_bam={
-            'bam': '{root}.bam',
-            'bam.bai': '{root}.bam.bai',
-        },
+        output_bam={'bam': '{root}.bam', 'bam.bai': '{root}.bam.bai'}
     )
 
-    # Use a set to track read names we've already "assigned" as primary
     job.command(f'''
         set -ex
         
-        cat > fix_primary.py << 'EOF'
+        # 1. Sort by NAME to group duplicates together
+        samtools sort -n -@ {job.cpu} -o name_sorted.bam {bam_localised.bam}
+
+        # 2. Process with Python
+        cat > fix_primary.py << EOF
 import pysam
 
-seen_primary_r1 = set()
-seen_primary_r2 = set()
+last_name = None
+r1_primary_found = False
+r2_primary_found = False
+modified_count = 0
+total_count = 0
 
-with pysam.AlignmentFile("{bam_localised}", "rb") as inf:
-    with pysam.AlignmentFile("{job.output_bam.bam}", "wb", template=inf) as outf:
+with pysam.AlignmentFile("name_sorted.bam", "rb") as inf:
+    with pysam.AlignmentFile("fixed_name_sorted.bam", "wb", template=inf) as outf:
         for read in inf:
-            # Skip reads that are already secondary or supplementary
-            if read.is_secondary or read.is_supplementary:
-                outf.write(read)
-                continue
+            total_count += 1
+            
+            # If we hit a new read name, reset our primary trackers
+            if read.query_name != last_name:
+                last_name = read.query_name
+                r1_primary_found = False
+                r2_primary_found = False
 
-            # Check if this is Read 1 or Read 2
-            current_set = seen_primary_r1 if read.is_read1 else seen_primary_r2
-
-            if read.query_name in current_set:
-                # We have already seen a primary for this name/pair-end.
-                # Convert this one to SUPPLEMENTARY (0x800)
-                read_type = "R1" if read.is_read1 else "R2"
-                print(f"Changed {{read.query_name}} ({{read_type}}) from primary to supplementary")
-                read.is_supplementary = True
-            else:
-                # First time seeing this name, keep it as Primary
-                current_set.add(read.query_name)
+            # We only care about reads not already marked secondary/supplementary
+            if not (read.is_secondary or read.is_supplementary):
+                if read.is_read1:
+                    if r1_primary_found:
+                        read.is_supplementary = True
+                        modified_count += 1
+                    else:
+                        r1_primary_found = True
+                else:
+                    if r2_primary_found:
+                        read.is_supplementary = True
+                        modified_count += 1
+                    else:
+                        r2_primary_found = True
 
             outf.write(read)
+
+        print(f"--- Processing Summary ---")
+        print(f"Total reads processed: {{total_count}}")
+        print(f"Reads converted to supplementary: {{modified_count}}")
 EOF
         
         # Run the Python script using the Python interpreter
         /usr/bin/env python fix_primary.py
         
-        # Index the output BAM
+        # 3. Restore Coordinate Sort order and Index
+        samtools sort -@ {job.cpu} -o {job.output_bam.bam} fixed_name_sorted.bam
         samtools index {job.output_bam.bam}
-    '''
-    )
+    ''')
 
     batch.write_output(job.output_bam, out_bam_path.removesuffix('.bam'))
     return job
