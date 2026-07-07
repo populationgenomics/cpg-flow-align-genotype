a97e37be128e4124ff421296a617094d20a949c5
MattWellie <mattwellie@gmail.com>
2026-07-07 15:33:26 +1000

    dupblaster needs tmpdir to exist prior to calling

--- src/align_genotype/jobs/align.py
+++ src/align_genotype/jobs/align.py
@@ -363,7 +363,7 @@ def dedup_sort_cmd(requested_nthreads: int, stats_path: str) -> str:
     nthreads = resources.STANDARD.request_resources(nthreads=requested_nthreads).get_nthreads()
     return dedent(
         f"""\
-    | dupblaster --stats {stats_path} --single-end-strategy picard-exact --tmp-dir $BATCH_TMPDIR/dupblaster \
+    | dupblaster --stats {stats_path} --single-end-strategy picard-exact --tmp-dir $BATCH_TMPDIR \
     | samtools sort -@{min(nthreads, 6) - 1} -T $BATCH_TMPDIR/samtools-dd-tmp -Obam
     """,
     ).strip()
