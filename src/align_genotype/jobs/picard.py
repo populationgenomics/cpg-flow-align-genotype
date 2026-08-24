d07d98e02cd88135a091eaaaae99775b624ad67a
EddieLF <eddieformaini@gmail.com>
2026-08-24 14:40:20 +1000

    Bump the --COVERAGE_CAP arg way higher

--- src/align_genotype/jobs/picard.py
+++ src/align_genotype/jobs/picard.py
@@ -229,15 +229,6 @@ def hs_metrics(
         -O $BATCH_TMPDIR/intervals.interval_list \\
         -SD {reference.dict}
 
-    # Convert CRAM to BAM for Picard CollectHsMetrics
-    # CRAMs written with htsjdk ~3.0.0 break Picard CollectHsMetrics, so we convert to BAM first
-    samtools view -b \\
-        -T {reference.base} \\
-        -o $BATCH_TMPDIR/input.bam \\
-        {cram_localised}
-
-    samtools index $BATCH_TMPDIR/input.bam
-
     picard {res.java_mem_options()} \\
       CollectHsMetrics \\
       -I $BATCH_TMPDIR/input.bam \\
@@ -245,6 +236,7 @@ def hs_metrics(
       --VALIDATION_STRINGENCY SILENT \\
       -TI $BATCH_TMPDIR/intervals.interval_list \\
       -BI $BATCH_TMPDIR/intervals.interval_list \\
+      --COVERAGE_CAP 1000000 \\
       -LEVEL null \\
       -LEVEL SAMPLE \\
       -LEVEL LIBRARY \\
